fcf508b104ac7c93524c8dfb149b95b8d0742828
Arman091 <rasoolarman7@gmail.com>
2023-09-18 11:29:01 +0530

    commit-1

---
+++ src/components/UI/Home.js
@@ -0,0 +1,16 @@
+import "./home.css"
+const Home = () => {
+    return (
+        <>
+            {/* rectangle-31 */}
+            <div className="header">
+           <h1>header content</h1>
+            </div>
+            <div className="banner-container">
+
+            </div>
+
+        </>
+    )
+}
+export default Home;
